db2359f253594914346655b4585d0713e3581189
Nguyen Do <dothnguyen@gmail.com>
2016-09-27 13:23:01 +1000

    add search query

--- themes/optimus-theme/functions.php
+++ themes/optimus-theme/functions.php
@@ -26,5 +26,186 @@ function advanced_search_query( $query ) {
         $query->set( 'meta_query', $meta_query );
         */
 
+
+        // search meta
+        $meta_query = array();
+
+
+        // list type
+        if (isset($_GET['list_type']) && 'all' != $_GET['list_type'] ) {
+            $meta_query[] = array(
+                'key'     => 'property_list_type',
+                'value'   => $_GET['list_type'],
+                'compare' => '='
+            );
+        }
+
+        // bedrooms
+        if (isset($_GET['bedrooms']) && !empty($_GET['bedrooms'])) {
+            if ($_GET['bedrooms'] < 5) {
+                $meta_query[] = array(
+                    'key'     => 'bedrooms',
+                    'value'   => $_GET['bedrooms'],
+                    'compare' => '='
+                );
+            } else {
+                $meta_query[] = array(
+                    'key'     => 'bedrooms',
+                    'value'   => 4,
+                    'compare' => '>'
+                );
+            }
+        }
+
+        // bathrooms
+        if (isset($_GET['bathrooms']) && !empty($_GET['bathrooms'])) {
+            if ($_GET['bathrooms'] < 3) {
+                $meta_query[] = array(
+                    'key'     => 'bathrooms',
+                    'value'   => $_GET['bathrooms'],
+                    'compare' => '='
+                );
+            } else {
+                $meta_query[] = array(
+                    'key'     => 'bathrooms',
+                    'value'   => 2,
+                    'compare' => '>'
+                );
+            }
+        }
+
+        // garages
+        if (isset($_GET['garages']) && !empty($_GET['garages'])) {
+            if ($_GET['garages'] < 3) {
+                $meta_query[] = array(
+                    'key'     => 'garages',
+                    'value'   => $_GET['garages'],
+                    'compare' => '='
+                );
+            } else {
+                $meta_query[] = array(
+                    'key'     => 'garages',
+                    'value'   => 2,
+                    'compare' => '>'
+                );
+            }
+        }
+
+        // price range
+        if (isset($_GET['price_range']) && !empty($_GET['price_range'])) {
+            $price_range = $_GET['price_range'];
+            switch ($price_range) {
+                case 1:
+                    // from $0 - $300
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 300,
+                        'compare' => '<='
+                    );
+                    break;
+                case 2:
+                    // from $301 - $700
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 301,
+                        'compare' => '>='
+                    );
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 700,
+                        'compare' => '<='
+                    );
+                    break;
+                case 3:
+                    // from 701 - 1500
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 701,
+                        'compare' => '>='
+                    );
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 1500,
+                        'compare' => '<='
+                    );
+                    break;
+                case 4:
+                    // from 1501 - 5000
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 1501,
+                        'compare' => '>='
+                    );
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 5000,
+                        'compare' => '<='
+                    );
+                    break;
+                case 5:
+                    // from 5001 - 50000
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 5001,
+                        'compare' => '>='
+                    );
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 50000,
+                        'compare' => '<='
+                    );
+                    break;
+                case 6:
+                    // from 50001 - 300000
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 50001,
+                        'compare' => '>='
+                    );
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 300000,
+                        'compare' => '<='
+                    );
+                    break;
+                case 7:
+                    // from 300001 - 700000
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 300001,
+                        'compare' => '>='
+                    );
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 700000,
+                        'compare' => '<='
+                    );
+                    break;
+                case 8:
+                    // from 700001 - 1500000
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 700001,
+                        'compare' => '>='
+                    );
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 1500000,
+                        'compare' => '<='
+                    );
+                    break;
+                case 9:
+                    // from 700001 - 1500000
+                    $meta_query[] = array(
+                        'key'     => 'price',
+                        'value'   => 1500000,
+                        'compare' => '>='
+                    );
+                    break;
+            }
+        }
+
+        $query->set( 'meta_query', $meta_query );
+
     }
 }
--- themes/optimus-theme/searchform-property.php
+++ themes/optimus-theme/searchform-property.php
@@ -24,10 +24,10 @@
   <div class="row">
     <div class="col-md-4 col-sm-6 col-xs-12">
       <div class="form-group">
-        <select class="form-control" id="select-type">
-          <option>Rent or sale</option>
-          <option>Rent</option>
-          <option>Sale</option>
+        <select class="form-control" id="select-type" name="list_type">
+          <option value="all">Rent or sale</option>
+          <option value="Rent" <?php if ($_GET['list_type'] == 'Rent') echo 'selected'; ?>>Rent</option>
+          <option value="Sale" <?php if ($_GET['list_type'] == 'Sale') echo 'selected'; ?>>Sale</option>
         </select>
       </div>
     </div>
@@ -44,45 +44,49 @@
     </div>
     <div class="col-md-4 col-sm-6 col-xs-12">
       <div class="form-group">
-        <select class="form-control" id="selectPriceMax">
-          <option>Price max.</option>
-          <option>100 000</option>
-          <option>150 000</option>
-          <option>200 000</option>
-          <option>300 000</option>
+        <select class="form-control" id="selectPriceRange" name="price_range">
+          <option value="">Price range</option>
+          <option value="1" <?php if ($_GET['price_range'] == '1') echo 'selected'; ?>>$0 - $300</option>
+          <option value="2" <?php if ($_GET['price_range'] == '2') echo 'selected'; ?>>$301 - $700</option>
+          <option value="3" <?php if ($_GET['price_range'] == '3') echo 'selected'; ?>>$701 - $1,500</option>
+          <option value="4" <?php if ($_GET['price_range'] == '4') echo 'selected'; ?>>$1,501 - $5,000</option>
+          <option value="5" <?php if ($_GET['price_range'] == '5') echo 'selected'; ?>>$5,001 - $50,000</option>
+          <option value="6" <?php if ($_GET['price_range'] == '6') echo 'selected'; ?>>$50,001 - $300,000</option>
+          <option value="7" <?php if ($_GET['price_range'] == '7') echo 'selected'; ?>>$300,001 - $700,000</option>
+          <option value="8" <?php if ($_GET['price_range'] == '8') echo 'selected'; ?>>$700,001 - $1,500,000</option>
+          <option value="9" <?php if ($_GET['price_range'] == '9') echo 'selected'; ?>>More than $1,500,000</option>
         </select>
       </div>
     </div>
     <div class="col-md-3 col-sm-6 col-xs-12">
       <div class="form-group">
-        <select class="form-control" id="selectBedrooms">
-          <option>Bedrooms</option>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
+        <select class="form-control" id="selectBedrooms" name="bedrooms">
+          <option value="">Bedrooms</option>
+          <option value="1" <?php if ($_GET['bedrooms'] == '1') echo 'selected'; ?>>1</option>
+          <option value="2" <?php if ($_GET['bedrooms'] == '2') echo 'selected'; ?>>2</option>
+          <option value="3" <?php if ($_GET['bedrooms'] == '3') echo 'selected'; ?>>3</option>
+          <option value="4" <?php if ($_GET['bedrooms'] == '4') echo 'selected'; ?>>4</option>
+          <option value="5" <?php if ($_GET['bedrooms'] == '5') echo 'selected'; ?>>More than 4</option>
         </select>
       </div>
     </div>
     <div class="col-md-3 col-sm-6 col-xs-12">
       <div class="form-group">
-        <select class="form-control" id="selectBathrooms">
-          <option>Bathrooms</option>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
+        <select class="form-control" id="selectBathrooms" name="bathrooms">
+          <option value="">Bathrooms</option>
+          <option value="1" <?php if ($_GET['bathrooms'] == '1') echo 'selected'; ?>>1</option>
+          <option value="2" <?php if ($_GET['bathrooms'] == '2') echo 'selected'; ?>>2</option>
+          <option value="3" <?php if ($_GET['bathrooms'] == '3') echo 'selected'; ?>>More than 2</option>
         </select>
       </div>
     </div>
     <div class="col-md-3 col-sm-6 col-xs-12">
       <div class="form-group">
-        <select class="form-control" id="selectParking">
-          <option>Parking space</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+        <select class="form-control" id="selectParking" name="garages">
+          <option value="">Parking space</option>
+          <option value="1" <?php if ($_GET['garages'] == '1') echo 'selected'; ?>>1</option>
+          <option value="2" <?php if ($_GET['garages'] == '2') echo 'selected'; ?>>2</option>
+          <option value="3" <?php if ($_GET['garages'] == '3') echo 'selected'; ?>>More than 2</option>
         </select>
       </div>
     </div>
